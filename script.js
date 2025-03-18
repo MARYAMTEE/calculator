ddf2943677d94c52093382430e95adee80f45905
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-03-18 17:15:36 +0100

    update style and javascript

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+	display.value += input;
+}
+
+function calculate() {
+	try{
+		display.value = eval(display.value);
+	}
+	catch(error){
+		display.value = "Error";
+	}
+}
+
+function clearDisplay(){
+	display.value = "";
+}
